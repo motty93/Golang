7a0c980eada917f27bfeb65db0ebcf34e2d034ea
motty93 <rdwbocungelt5@gmail.com>
2020-07-29 22:30:02 +0900

    add: for array slice map

--- tohoho/for.go
+++ tohoho/for.go
@@ -14,4 +14,18 @@ func main() {
 			fmt.Println(n)
 		}
 	}
+
+	// 配列・スライス・mapなどのイテラブルな物に対するforループ
+	colors := [...]string{"red", "green", "blue"}
+	for i, color := range colors {
+		fmt.Printf("%d: %s\n", i, color)
+	}
+
+	foods := map[int]string{
+		10: "apple",
+		11: "orange",
+	}
+	for k, v := range foods {
+		fmt.Printf("%d=%s\n", k, v)
+	}
 }
