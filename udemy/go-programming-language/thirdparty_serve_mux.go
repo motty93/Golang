5f8cb542ef3545c900a167e70006675ae3ff1b72
motty93 <rdwbocungelt5@gmail.com>
2020-08-12 13:43:34 +0900

    update: http index

--- udemy/go-programming-language/thirdparty_serve_mux.go
+++ udemy/go-programming-language/thirdparty_serve_mux.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"text/template"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-var tpl *tmplate.Template
+var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
@@ -14,17 +17,25 @@ func init() {
 func main() {
 	mux := httprouter.New()
 	mux.GET("/", index)
-	mux.GET("/about", about)
-	mux.GET("/contact", contact)
-	mux.GET("/apply", apply)
-	mux.POST("/apply", applyProcess)
-	mux.GET("/user/:name/exact", user)
-	mux.GET("/blog/:category/:article", blogRead)
-	mux.POST("/blog/:category/:article", blogWrite)
+	// mux.GET("/about", about)
+	// mux.GET("/contact", contact)
+	// mux.GET("/apply", apply)
+	// mux.POST("/apply", applyProcess)
+	// mux.GET("/user/:name/exact", user)
+	// mux.GET("/blog/:category/:article", blogRead)
+	// mux.POST("/blog/:category/:article", blogWrite)
 	http.ListenAndServe(":8080", mux)
 }
 
 func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
 	HandleError(w, err)
+}
+
+// HandleError is http response error function
+func HandleError(w http.ResponseWriter, e error) {
+	if e != nil {
+		http.Error(w, e.Error(), http.StatusInternalServerError)
+		log.Println(e)
+	}
 }
