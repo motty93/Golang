651b7b1e4654003ecf5f30a6f92314717f58416d
motty93 <rdwbocungelt5@gmail.com>
2020-08-18 21:44:51 +0900

    add: interface

---
+++ udemy/getting-started-bitcoins-fintech/interface.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Human interface {
+	Say() string // human.Say() used as value
+}
+
+type Person struct {
+	Name string
+}
+
+type Dog struct {
+	Name string
+}
+
+func (p *Person) Say() string {
+	p.Name = "Mr." + p.Name
+	fmt.Println(p.Name)
+	return p.Name
+}
+
+func DriveCar(human Human) {
+	if human.Say() == "Mr.Mike" {
+		fmt.Println("Run")
+	} else {
+		fmt.Println("Get out")
+	}
+}
+
+func main() {
+	var mike Human = &Person{"Mike"}
+	var x Human = &Person{"x"}
+	var john Dog = Dog{"john"}
+	DriveCar(mike)
+	DriveCar(x)
+	// error
+	DriveCar(john)
+}
