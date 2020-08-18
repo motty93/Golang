531fc15da14a45982a6964f947f7f5b86b512496
motty93 <rdwbocungelt5@gmail.com>
2020-08-18 21:54:33 +0900

    add: stringer comment

--- udemy/getting-started-bitcoins-fintech/stringer.go
+++ udemy/getting-started-bitcoins-fintech/stringer.go
@@ -7,6 +7,7 @@ type Person struct {
 	Age  int
 }
 
+// structをPrintlnした際にageまでも表示したくない場合、String()インスタンスメソッドを実装すると隠せる
 func (p Person) String() string {
 	return fmt.Sprintf("my name is %v.", p.Name)
 }
