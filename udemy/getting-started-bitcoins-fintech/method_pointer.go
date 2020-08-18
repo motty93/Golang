53491cd74706836a8398a23f00faf913368a165b
motty93 <rdwbocungelt5@gmail.com>
2020-08-18 21:10:20 +0900

    add: pointer method

---
+++ udemy/getting-started-bitcoins-fintech/method_pointer.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+}
+
+// 値レシーバ
+func (v Vertex) Area() int {
+	return v.X * v.Y
+}
+
+func Area(v Vertex) int {
+	return v.X * v.Y
+}
+
+// pointerレシーバ
+// pointerにすると中身を書き換えることができる
+func (v *Vertex) Scale(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
+
+func main() {
+	v := Vertex{3, 4}
+	// fmt.Println(Area(v))
+	v.Scale(10)
+	fmt.Println(v.Area())
+}
