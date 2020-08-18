9842a343401de03d8b055d569e59771a52b2133f
motty93 <rdwbocungelt5@gmail.com>
2020-08-18 21:15:07 +0900

    update: design pattern new struct

--- udemy/getting-started-bitcoins-fintech/method_pointer.go
+++ udemy/getting-started-bitcoins-fintech/method_pointer.go
@@ -3,28 +3,34 @@ package main
 import "fmt"
 
 type Vertex struct {
-	X, Y int
+	x, y int
 }
 
 // 値レシーバ
 func (v Vertex) Area() int {
-	return v.X * v.Y
+	return v.x * v.y
 }
 
-func Area(v Vertex) int {
-	return v.X * v.Y
-}
+// func Area(v Vertex) int {
+// 	return v.x * v.y
+// }
 
 // pointerレシーバ
 // pointerにすると中身を書き換えることができる
 func (v *Vertex) Scale(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
+	v.x = v.x * i
+	v.y = v.y * i
+}
+
+// golangのデザインパターン
+func New(x, y int) *Vertex {
+	return &Vertex{x, y}
 }
 
 func main() {
-	v := Vertex{3, 4}
 	// fmt.Println(Area(v))
+	// v := Vertex{3, 4}
+	v := New(3, 4)
 	v.Scale(10)
 	fmt.Println(v.Area())
 }
