2f4add313c6016d854e12a18588b3a54d0bb9eaf
motty93 <rdwbocungelt5@gmail.com>
2020-08-18 15:53:57 +0900

    add: new and make

---
+++ udemy/getting-started-bitcoins-fintech/new_make.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var p *int = new(int)
+	// fmt.Println(*p)
+	// *p++
+	// fmt.Println(*p)
+
+	// var p2 *int
+	// fmt.Println(p2)
+	// // nilのものはまだメモリが確保されてないのでpanic
+	// *p2++
+	// fmt.Println(p2)
+
+	s := make([]int, 0)
+	fmt.Printf("%T\n", s)
+
+	m := make(map[string]int)
+	fmt.Printf("%T\n", m)
+
+	ch := make(chan int)
+	fmt.Printf("%T\n", ch)
+
+	var p *int = new(int)
+	fmt.Printf("%T\n", p)
+
+	var st = new(struct{})
+	fmt.Printf("%T\n", st)
+}
