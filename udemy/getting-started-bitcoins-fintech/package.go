fe39975640b33c678bd00f479c34bde591da0051
motty93 <rdwbocungelt5@gmail.com>
2020-08-19 17:22:20 +0900

    add: use package

---
+++ udemy/getting-started-bitcoins-fintech/package.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
+)
+
+func main() {
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2018-01-01", "2018-12-31", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
+}
