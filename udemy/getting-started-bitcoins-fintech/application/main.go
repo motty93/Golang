c40b9ea5bc87ae782944f9f954c8bd133212db7c
motty93 <rdwbocungelt5@gmail.com>
2020-08-24 21:59:36 +0900

    add: http request save edit view regexp

--- udemy/getting-started-bitcoins-fintech/application/main.go
+++ udemy/getting-started-bitcoins-fintech/application/main.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 	"text/template"
 )
 
@@ -32,15 +33,17 @@ var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	// t, _ := template.ParseFiles(tmpl + ".html")
 	// t.Execute(w, p)
+
+	// 上のtempalteを使用
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	// /view/test
-	title := r.URL.Path[len("/view/"):]
+	// title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -49,8 +52,8 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHander(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHander(w http.ResponseWriter, r *http.Request, title string) {
+	// title := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -58,8 +61,8 @@ func editHander(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHander(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHander(w http.ResponseWriter, r *http.Request, title string) {
+	// title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body") //textareaのname="body"を取得
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -70,6 +73,19 @@ func saveHander(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+var validPath = regexp.MustCompile("^/(edit|view|save)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
 func main() {
 	// p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
 	// p1.save()
@@ -77,9 +93,9 @@ func main() {
 	// p2, _ := loadPage(p1.Title)
 	// fmt.Println(string(p2.Body))
 
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHander)
-	http.HandleFunc("/save/", saveHander)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHander))
+	http.HandleFunc("/save/", makeHandler(saveHander))
 	// nilはrootや存在しないパスに対してアクセスするとnot foundを返す
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
