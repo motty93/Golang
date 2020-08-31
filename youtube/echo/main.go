1c84e12321c2fff40e3e047b703581ad5f3e7d1f
motty93 <rdwbocungelt5@gmail.com>
2020-08-31 23:16:45 +0900

    add: echo youtube

--- youtube/echo/main.go
+++ youtube/echo/main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 )
 
-// type Handler interface {
-// 	ServeHTTP(ResponseWriter, *Request)
-// }
-
-func ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/dog":
-		io.WriteString(w, "dog dog dog")
-	case "/":
-		fmt.Fprintf(w, htmlTemplate("root", "<h1>Hello Golang</h1>"))
-	}
+type ResponseData struct {
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+	Response int    `json:"status"`
 }
 
-func login(w http.ResponseWriter, req *http.Request) {
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, htmlTemplate("hello golang", "<h1>hellor world</h1>"))
+}
+
+func loginHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, htmlTemplate("login", "<h1>enter your username and password</h1>"))
 }
 
-func welcome(w http.ResponseWriter, req *http.Request) {
+func welcomeHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, htmlTemplate("welcome", "<h1>welcome</h1>"))
+}
+
+func responseHandler(w http.ResponseWriter, req *http.Request) {
+	data := ResponseData{"motty", 40, http.StatusOK}
+	res, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(res))
 }
 
 func htmlTemplate(title, body string) (template string) {
@@ -46,10 +55,11 @@ func htmlTemplate(title, body string) (template string) {
 
 func main() {
 	// http.Handle("/", http.HandlerFunc(login))と同じ
-	http.HandleFunc("/", login)
-	http.HandleFunc("/login", login)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/response", responseHandler)
 	// http.HandleFunc("/welcome", welcome)
-	http.HandleFunc("/welcome/", welcome) //最後にスラッシュを入れると/welcome/testでも普通に表示される
+	http.HandleFunc("/welcome/", welcomeHandler) //最後にスラッシュを入れると/welcome/testでも普通に表示される
 
 	// http.ListenAndServe("localhost:8080", http.HandlerFunc(ServeHTTP))
 	http.ListenAndServe("localhost:8080", nil) //nilは何もなければ404を返す
