088a629d6854ad3a05eca9de99e48d5b509b644d
motty93 <rdwbocungelt5@gmail.com>
2020-09-02 14:54:26 +0900

    update: return json marshal & json map

--- youtube/echo/main.go
+++ youtube/echo/main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
+type GetResonseData struct {
+	Status int    `json:"status"`
+	Body   string `json:"body"`
+}
+
 func main() {
 	e := echo.New()
 
+	data := GetResonseData{
+		Status: http.StatusOK,
+		Body:   "hello echo",
+	}
+	res, err := json.Marshal(data)
+	if err != nil {
+		data.Status = http.StatusInternalServerError
+	}
+
+	resMap := map[string]interface{}{
+		"status": http.StatusOK,
+		"body":   "hello echo",
+	}
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Well, hello there !!")
+		return c.JSON(http.StatusOK, resMap)
 	})
+	e.GET("/struct", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, string(res))
+	})
+
+	e.Start(":8080")
 }
