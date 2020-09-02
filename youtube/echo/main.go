aa1ec20744397a6f1584d28b85d2e2ab773dcadc
motty93 <rdwbocungelt5@gmail.com>
2020-09-02 15:08:59 +0900

    add: e.Logger

--- youtube/echo/main.go
+++ youtube/echo/main.go
@@ -39,8 +39,12 @@ func structJsonMarshalResponse(c echo.Context) error {
 func main() {
 	e := echo.New()
 
-	e.GET("/", mapResponse)
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "hello echo framework")
+	})
+	e.GET("/map", mapResponse)
 	e.GET("/struct", structJsonMarshalResponse)
 
-	e.Start(":8080")
+	e.Logger.Print("Listening on port 8080...")
+	e.Logger.Fatal(e.Start(":8080"))
 }
