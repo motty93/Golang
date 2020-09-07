d681319dc3917b5244329133459144bcf8db2b1b
motty93 <rdwbocungelt5@gmail.com>
2020-09-07 22:05:58 +0900

    add: config go

---
+++ youtube/echo/server/config.go
@@ -0,0 +1,11 @@
+package server
+
+type ConfigDatabase struct {
+	AppName  string `env:"APP_NAME" env-default:"SERVER"`
+	AppEnv   string `env:"APP_ENV" env-default:"DEV"`
+	Port     string `env:"MY_APP_PORT" env-default:"8080"`
+	Host     string `env:"HOST" env-default:"localhost"`
+	LogLevel string `env:"LOG_LEVEL" env-default:"ERROR"`
+}
+
+var cfg ConfigDatabase
