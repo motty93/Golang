64a4b53a5f84efe7506433b47386fdb79beccb22
motty93 <rdwbocungelt5@gmail.com>
2020-09-14 10:03:34 +0900

    update: bson.M insert data

--- youtube/mongo/main.go
+++ youtube/mongo/main.go
@@ -11,12 +11,17 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// BSON = Binary Encoded Json. Includes additional ty pes e.g. int, long, date, floating point..
+// bson.D = ordered document   bson.D{{"hello", "world"}, {"foo", "bar"}}
+// bson.M = unordered document/map   bson.M{"hello": "world", "foo": "bar"}
+// bson.A = array   bson.A{"Eric", "kyle"}
+// bson.E = usually userd as an element inside bson.D
 type Product struct {
 	ID          primitive.ObjectID `json:"_id" bson:"_id"`
 	Name        string             `json:"product_name" bson:"product_name"`
 	Price       int                `json:"price" bson:"price"`
 	Currency    string             `json:"currency" bson:"currency"`
-	Quantity    string             `json:"quantity" bson:"quantity"`
+	Quantity    int                `json:"quantity" bson:"quantity"`
 	Discount    int                `json:"discount" bson:"discount"`
 	Vendor      string             `json:"vendor" bson:"vendor"`
 	Accessories []string           `json:"accessories,omitempty" bson:"accessories,omitempty"`
@@ -28,7 +33,7 @@ var iphone10 = Product{
 	Name:        "iphone10",
 	Price:       900,
 	Currency:    "USD",
-	Quantity:    "40",
+	Quantity:    40,
 	Vendor:      "apple",
 	Accessories: []string{"charger", "headset", "slotopener"},
 	SkuID:       "1234",
@@ -40,10 +45,22 @@ var trimmer = Product{
 	Name:        "easy philips trimmer",
 	Price:       120,
 	Currency:    "USD",
-	Quantity:    "400",
+	Quantity:    400,
 	Vendor:      "Philips",
 	Accessories: []string{"charger", "comb", "bladeset", "cleaning oil"},
 	SkuID:       "2345",
+}
+
+var speaker = Product{
+	ID:          primitive.NewObjectID(),
+	Name:        "speakers",
+	Price:       300,
+	Currency:    "USD",
+	Quantity:    25,
+	Vendor:      "Bosch",
+	Discount:    4,
+	Accessories: []string{"cables", "remote"},
+	SkuID:       "3466",
 }
 
 func main() {
@@ -62,7 +79,18 @@ func main() {
 	// なければ作成する
 	db := client.Database("tronics")
 	collection := db.Collection("products")
-	res, err := collection.InsertOne(context.Background(), trimmer)
+	// res, err := collection.InsertOne(context.Background(), trimmer)
+	// res, err := collection.InsertOne(context.Background(), bson.D{
+	// 	{"name", "eric"},
+	// 	{"surname", "cartman"},
+	// 	{"hobbies", bson.A{"videogame", "alexa", "kfc"}},
+	// })
+	// bson.Mを使ってデータを入れる方法
+	// res, err := collection.InsertOne(context.Background(), bson.M{
+	// 	"name":    "eric",
+	// 	"surname": "cartman",
+	// 	"hobbies": bson.A{"videogame", "alexa", "kfc"},
+	// })
 
 	if err != nil {
 		fmt.Println(err)
