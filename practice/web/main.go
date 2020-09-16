d77905b97edbcb04a73f6813c5ccb05333402cca
motty93 <rdwbocungelt5@gmail.com>
2020-09-16 14:47:39 +0900

    update: practice http

--- practice/web/main.go
+++ practice/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -12,11 +13,21 @@ type Article struct {
 	Body  string
 }
 
+type Product struct {
+	Name     string `json:"name"`
+	Price    int    `json:"price"`
+	Quantity int    `json:"quantity"`
+}
+
 var tpl *template.Template
+
+func safeHTMLTemplate(text string) template.HTML {
+	return template.HTML(text)
+}
 
 func init() {
 	funcMap := template.FuncMap{
-		"safehtml": func(text string) template.HTML { return template.HTML(text) },
+		"safehtml": safeHTMLTemplate,
 	}
 	tpl = template.Must(template.New("").Funcs(funcMap).ParseFiles("template.html"))
 }
@@ -36,10 +47,45 @@ func dogHandler(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(rw, "<h1>dogs</h1><h2>dog dog dog</h2>")
 }
 
+func jsonResponseHandler(rw http.ResponseWriter, req *http.Request) {
+	data := map[string]interface{}{
+		"message": "hello golang",
+		"status":  http.StatusOK,
+	}
+
+	// map to json
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprint(rw, string(bytes))
+}
+
+func productResponseHandler(rw http.ResponseWriter, req *http.Request) {
+	// product := Product{"商品A", 100, 10}
+	product := Product{
+		Name:     "商品A",
+		Price:    100,
+		Quantity: 10,
+	}
+
+	// struct to json byte
+	bytes, err := json.Marshal(product)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(rw, string(bytes))
+}
+
 func main() {
 	// http.Handle("/hello", http.HandlerFunc(helloHandler))
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/dog", dogHandler)
+	http.HandleFunc("/json", jsonResponseHandler)
+	http.HandleFunc("/product", productResponseHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
