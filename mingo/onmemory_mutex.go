350c43d5e4d4138aef422c2d06041329e6630a80
motty93 <rdwbocungelt5@gmail.com>
2021-03-08 14:40:42 +0900

    feat: map mutex lock

---
+++ mingo/onmemory_mutex.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type KeyValue struct {
+	store map[string]string // key_valueを格納するためのmap
+	mu    sync.RWMutex      //排他処理のためのmutex
+}
+
+func NewKeyValue() *KeyValue {
+	return &KeyValue{store: make(map[string]string)}
+}
+
+func (kv *KeyValue) Set(key, val string) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.store[key] = val
+}
+
+func (kv *KeyValue) Get(key string) (string, bool) {
+	kv.mu.RLock()
+	defer kv.mu.RUnlock()
+	val, ok := kv.store[key]
+
+	return val, ok
+}
+
+func main() {
+	kv := NewKeyValue()
+	kv.Set("key", "value")
+	value, ok := kv.Get("key")
+	if ok {
+		fmt.Println(value)
+	}
+}
