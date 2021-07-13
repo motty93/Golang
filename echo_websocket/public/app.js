6bf8ee52efef50624f48043f49ef5df8f432b745
motty93 <rdwbocungelt5@gmail.com>
2021-07-13 20:27:24 +0900

    feat: client側の処理変更

--- echo_websocket/public/app.js
+++ echo_websocket/public/app.js
@@ -1,28 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let loc = window.location;
-  let uri = 'ws:';
+  // let loc = window.location;
+  // let uri = 'ws:';
+  //
+  // if (loc.protocol === 'https:') {
+  //   uri = 'wss:';
+  // }
+  //
+  // uri += '//' + loc.host;
+  // uri += loc.pathname + 'ws';
+  let uri = 'ws://localhost:8080/ws'
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+  const btn = document.querySelector('.btn');
+  const ws = new WebSocket(uri);
 
-  if (loc.protocol === 'https:') {
-    uri = 'wss:';
-  }
-
-  uri += '//' + loc.host;
-  uri += loc.pathname + 'ws';
-
-  ws = new WebSocket(uri);
   ws.onopen = function() {
     console.log('Connected');
   }
 
   ws.onmessage = function(e) {
-    const el = document.getElementById('output');
-    el.innerHTML += e.data + '<br>';
+    output.innerHTML += e.data + '<br>';
   }
 
-  const btn = document.querySelector('.btn');
   btn.addEventListener('click', () => {
-    const value = document.getElementById('input').value;
-    ws.send(value);
-    document.getElementById('input').value = '';
+    ws.send(input.value);
+    input.value = '';
   })
 });
