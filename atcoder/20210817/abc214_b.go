064ac0b2e25044e60bc6049acd5c982166f32fe8
motty93 <rdwbocungelt5@gmail.com>
2021-08-17 10:56:44 +0900

    feat: 最適化

--- atcoder/20210817/abc214_b.go
+++ atcoder/20210817/abc214_b.go
@@ -10,8 +10,8 @@ func main() {
 
 	count := 0
 	for a := 0; a < s+1; a++ {
-		for b := 0; b < s+1; b++ {
-			for c := 0; c < s+1; c++ {
+		for b := 0; b < s+1-a; b++ {
+			for c := 0; c < s+1-c-b; c++ {
 				if a+b+c <= s && a*b*c <= t {
 					count++
 				}
