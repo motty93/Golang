54a40bfa98e29b01d8c94f5cb05f03e3c08940e6
motty93 <rdwbocungelt5@gmail.com>
2021-09-08 10:36:58 +0900

    feat: atcoder 167 b

---
+++ atcoder/20210908/abc167_b.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const BUFSIZE = 10000000
+
+var rdr *bufio.Reader
+
+func stoi(s string) int {
+	r, _ := strconv.Atoi(s)
+	return r
+}
+
+func readline() string {
+	buf := make([]byte, 0, 16)
+	for {
+		line, prefix, _ := rdr.ReadLine()
+		buf = append(buf, line...)
+		if !prefix {
+			break
+		}
+	}
+
+	return string(buf)
+}
+
+func readIntSlice() []int {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, stoi(v))
+	}
+	return slice
+}
+
+func init() {
+	rdr = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+}
+
+func main() {
+	d := readIntSlice()
+
+	if d[0] >= d[3] {
+		fmt.Println(d[3])
+	} else if (d[3] - d[0]) <= d[1] {
+		fmt.Println(d[0])
+	} else {
+		fmt.Println(d[0] - (d[3] - d[0] - d[1]))
+	}
+}
